e82e19e990000f89de1edcfe4c75a5e876220966
shafik720 <shafikrasel5@gmail.com>
2023-08-14 18:37:17 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Routes } from './Utilites/Routes/Routes.jsx'
@@ -8,5 +7,5 @@ import { Routes } from './Utilites/Routes/Routes.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
         <RouterProvider router={Routes} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
