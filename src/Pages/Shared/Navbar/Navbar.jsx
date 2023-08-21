d4aca19f2b809b9b23fbdb61a168c4412fcb57da
shafik720 <shafikrasel5@gmail.com>
2023-08-21 11:05:30 +0600

    navbar added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import './Navbar.css';
 const Navbar = () => {
     return (
-        <div>
+        <div className='menu-div px-5'>
             <div className="menubar flex justify-between items-center">
                 <div className="menubar-left">
                     <h2 className="text-4xl font-bold">Food Lab</h2>
