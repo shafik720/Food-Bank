dcfdd01736bcd44ac0d38f26f510b784b8afc628
shafik720 <shafikrasel5@gmail.com>
2023-08-26 21:34:01 +0600

    making menu responsive

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import './Navbar.css';
 import menu1 from '../../../assets/img/menu.png';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [isActive, setIsActive] = useState(false);
+    console.log(isActive);
+
     return (
         <div className='menu-div px-5'>
-            <div className="mobile-menu-icons">
-                <img src={menu1} alt="" />
+            <div className={`mobile-menu-icons ${isActive ? 'active' : ''}`}>
+                <img src={menu1} alt="" draggable onClick={()=>setIsActive(!isActive)} />
             </div>
-            <div className="menubar max-md:flex-col max-md:justify-center flex justify-between items-center ">
+            <div className={`menubar max-md:flex-col max-md:justify-center flex justify-between items-center ${isActive ? 'active' : ''}`}>
                 <div className="menubar-left">
                     <h2 className="text-4xl font-bold max-md:hidden ">Food Lab</h2>
                 </div>
