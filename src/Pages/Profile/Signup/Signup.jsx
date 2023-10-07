4a26dfcf5a38ed3f4969d9d04f41229b7f9d0293
shafik720 <shafikrasel5@gmail.com>
2023-10-07 08:03:51 +0600

    added style for social sign in button and added fav icon

--- src/Pages/Profile/Signup/Signup.jsx
+++ src/Pages/Profile/Signup/Signup.jsx
@@ -1,14 +1,14 @@
 import { Helmet } from "react-helmet-async";
-import './Signup.css' ; 
+import './Signup.css';
 
 const Signup = () => {
     return (
         <div className="signup-div flex flex-col justify-center items-center md:py-40  ">
             <Helmet>
                 <title>Sign Up</title>
-                <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+                <link rel="icon" type="image/svg+xml" href="https://i.ibb.co/c3LLwqk/plate.png" />
             </Helmet>
-            <div className="form-div border border-slate-50 p-8 text-white lg:w-[450px]">
+            <div className="form-div border border-slate-50 p-8 text-white md:w-[450px] lg:w-[450px] w-11/12">
                 <h2 className="text-4xl text-center mb-10"> Sign Up </h2>
                 <div className="form-fields flex flex-col gap-y-5 mt-3 text-black">
                     <input className="max-w-full py-2 px-4 rounded text-lg focus:outline-emerald-500" type="text" name="" id="" placeholder="Your Name" />
@@ -22,14 +22,14 @@ const Signup = () => {
 
             {/* --- Social Sign In Icons --- */}
             <div className="social-button flex gap-7 ">
-                <div className="social-icons border border-slate-50 py-4 px-5 cursor-pointer">
-                    <img style={{width:'42px'}} src="https://i.ibb.co/0sh2Gtn/google-1.png" alt="" />
+                <div className="social-icons border border-slate-50 w-14 h-14 flex justify-center items-center cursor-pointer bg-white">
+                    <img style={{ width: '42px' }} src="https://i.ibb.co/0sh2Gtn/google-1.png" alt="" />
                 </div>
-                <div className="social-icons border border-slate-50 py-4 px-5 cursor-pointer">
-                    <img style={{width:'42px'}} src="https://i.ibb.co/tB9HhgW/facebook.png" alt="" />
+                <div className="social-icons border border-slate-50 w-14 h-14 flex justify-center items-center  cursor-pointer bg-white">
+                    <img style={{ width: '42px' }} src="https://i.ibb.co/tB9HhgW/facebook.png" alt="" />
                 </div>
-                <div className="social-icons border border-slate-50 py-4 px-5 cursor-pointer">
-                    <img style={{width:'42px'}} src="https://i.ibb.co/xsmwmm7/github.png" alt="" />
+                <div className="social-icons border border-slate-50 w-14 h-14 flex justify-center items-center cursor-pointer bg-white">
+                    <img style={{ width: '42px' }} src="https://i.ibb.co/xsmwmm7/github.png" alt="" />
                 </div>
             </div>
         </div>
