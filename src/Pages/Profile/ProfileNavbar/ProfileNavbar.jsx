38434c583f25fa60f4b1aa936e16ae59a133a086
shafik720 <shafikrasel5@gmail.com>
2023-10-14 00:00:48 +0600

    creating sidebar menu for profile section

--- src/Pages/Profile/ProfileNavbar/ProfileNavbar.jsx
+++ src/Pages/Profile/ProfileNavbar/ProfileNavbar.jsx
@@ -4,10 +4,12 @@ const ProfileNavbar = () => {
     return (
         <div className="profile-navbar-div">
             <ul>
-                <li>Home</li>
-                <li>About</li>
+                <li className='flex flex-col justify-center items-center gap-2 menu-active relative'> <span className='menu-outer-span'> <span className="menu-inner-span"><img src="https://i.ibb.co/g6pKwGc/cutlery-1.png" alt="" /></span></span> <span className='menu-text'>Home</span></li>
+                <li className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span"><img src="https://i.ibb.co/g6pKwGc/cutlery-1.png" alt="" /></span></span> <span className='menu-text'>Home</span></li>
+                <li className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span"><img src="https://i.ibb.co/g6pKwGc/cutlery-1.png" alt="" /></span></span> <span className='menu-text'>Home</span></li>
+                {/* <li>About</li>
                 <li>Menu</li>
-                <li>Contact</li>
+                <li>Contact</li> */}
             </ul>
         </div>
     );
