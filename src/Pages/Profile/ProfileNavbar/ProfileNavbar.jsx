41cd2fdac35b0837026d45e853dc0b309543d280
shafik720 <shafikrasel5@gmail.com>
2023-10-14 23:46:58 +0600

    bug fixed for profile menubar style

--- src/Pages/Profile/ProfileNavbar/ProfileNavbar.jsx
+++ src/Pages/Profile/ProfileNavbar/ProfileNavbar.jsx
@@ -13,7 +13,7 @@ const ProfileNavbar = () => {
             <ul>
                 {/* <NavLink to='/profile/profileInfo' activeClassName="active">about</NavLink> */}
                 <li>
-                    <NavLink to='/profile/profileInfo' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span"><img src={user?.photoURL || 'https://i.ibb.co/g99nZMz/user.png'} alt="" /></span></span> <span className='menu-text'>Profile</span></NavLink>
+                    <NavLink to='/profile/profileInfo' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span special-radius1"><img src={user?.photoURL || 'https://i.ibb.co/g99nZMz/user.png'} alt="" /></span></span> <span className='menu-text'>Profile</span></NavLink>
                 </li>
                 <li>
                     <NavLink to='/profile/editProfile' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span special-radius2"><img src="https://i.ibb.co/5WDXr6R/user-avatar-1.png" alt="" /></span></span> <span className='menu-text'>Edit Profile</span></NavLink>
