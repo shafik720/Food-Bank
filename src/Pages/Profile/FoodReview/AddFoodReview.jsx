9a12298eb3821d63e2f30e1531ce2323bcf7180b
shafik720 <shafikrasel5@gmail.com>
2023-10-25 02:51:50 +0600

    get selected country name after clicking submit review button

--- src/Pages/Profile/FoodReview/AddFoodReview.jsx
+++ src/Pages/Profile/FoodReview/AddFoodReview.jsx
@@ -8,6 +8,10 @@ const AddFoodReview = () => {
     const [selectedCountry, setSelectedCountry] = useState(0);
     const [selectedState, setSelectedState] = useState(0)
 
+    const handleReviewSubmit = (e) => {
+        e.preventDefault();
+        console.log(selectedCountry);
+    }
 
     return (
         <div className="container add-food-review w-full">
@@ -15,39 +19,46 @@ const AddFoodReview = () => {
             <hr className="horizontal-for-profile-section" />
 
             <div className="mx-auto lg:max-w-2xl p-4">
-                <CountrySelection onCountryChange={setSelectedCountry} />
-                <StateSelection countryId={selectedCountry} onStateChange={setSelectedState} />
-                <CitySelection stateId={selectedState} />
+                <form action="" onSubmit={handleReviewSubmit}>
+                    <CountrySelection onCountryChange={setSelectedCountry} />
+                    <StateSelection countryId={selectedCountry} onStateChange={setSelectedState} />
+                    <CitySelection stateId={selectedState} />
 
-                {/* --- Select Restaurant Name --- */}
-                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                    <p className="font-semibold text-slate-800 text-center">Restaurant Name : </p>
-                    <span>
-                        <input type="text" placeholder="Type Restaurant Name Here ..." className="input input-success w-full max-w-xs " />
-                    </span>
-                </div>
+                    {/* --- Select Restaurant Name --- */}
+                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                        <p className="font-semibold text-slate-800 text-center">Restaurant Name : </p>
+                        <span>
+                            <input required type="text" placeholder="Type Restaurant Name Here " className="input input-success w-full max-w-xs  rounded-sm" />
+                        </span>
+                    </div>
 
-                {/* --- Select Food Name --- */}
-                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                    <p className="font-semibold text-slate-800 text-center">Food Name : </p>
-                    <span>
-                        <input type="text" placeholder="Type Food Name Here ... " className="input input-success w-full max-w-xs " />
-                    </span>
-                </div>
+                    {/* --- Select Food Name --- */}
+                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                        <p className="font-semibold text-slate-800 text-center">Food Name : </p>
+                        <span>
+                            <input required type="text" placeholder="Type Food Name Here  " className="input input-success w-full max-w-xs rounded-sm" />
+                        </span>
+                    </div>
 
-                {/* --- Select Rating --- */}
-                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                    <p className="font-semibold text-slate-800 text-center">Give Your Rating : </p>
-                    <span>
-                        <div className="rating rating-lg">
-                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400"  />
-                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                        </div>
-                    </span>
-                </div>
+                    {/* --- Select Rating --- */}
+                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                        <p className="font-semibold text-slate-800 text-center">Give Your Rating : </p>
+                        <span>
+                            <div className="rating rating-lg">
+                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                            </div>
+                        </span>
+                    </div>
+
+                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                        <div></div>
+                        <button className="btn btn-active btn-success w-52 hover:bg-red-800 hover:text-white border-none ">Submit Review</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
