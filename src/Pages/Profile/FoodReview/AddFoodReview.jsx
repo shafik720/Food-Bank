87883f5ffe58934e3be78f3b326c03825b5900ef
shafik720 <shafikrasel5@gmail.com>
2023-10-25 16:07:37 +0600

    trying rating impression ui

--- src/Pages/Profile/FoodReview/AddFoodReview.jsx
+++ src/Pages/Profile/FoodReview/AddFoodReview.jsx
@@ -5,71 +5,101 @@ import StateSelection from "../../../Utilites/Country&CitySelection/StateSelecti
 
 
 const AddFoodReview = () => {
-    const [selectedCountry, setSelectedCountry] = useState(0) ;
-    const [selectedState, setSelectedState] = useState(0) ; 
-    const [selectedCity, setSelectedCity] = useState(0) ;
 
+    // --- get country, state & city value
+    const [selectedCountry, setSelectedCountry] = useState(0);
+    const [selectedState, setSelectedState] = useState(0);
+    const [selectedCity, setSelectedCity] = useState(0);
+
+    // --- get restaturant and food name
     const [restaurant, setRestaurant] = useState('');
     const [foodname, setFoodname] = useState('');
 
+    // --- get the value of rating
+    const [rating, setRating] = useState(5);
+    // const [ratingImpression, setRatingImpression] = useState('');
+    let ratingImpression = null
+    const handleRating = (e) => {
+        const selectedRating = parseInt(e.target.value);
+        setRating(selectedRating)
+    }
+    useEffect(() => {
+        if (rating == 5) {
+            // setRatingImpression('Best In Town');
+            ratingImpression = <div><p>'Best In Town'</p></div>
+        } else if (rating == 1) {            
+            ratingImpression = <div><p>Best In Town</p></div>
+        }
+    }, [rating])
+
     const handleReviewSubmit = (e) => {
-        e.preventDefault();
-        console.log('Country Id : ',selectedCountry);
-        console.log('State Id : ',selectedState);
-        console.log('City Id : ',selectedCity);
-        console.log('Restaurant Name : ', restaurant);
-        console.log('Food Name : ', foodname);
+                        e.preventDefault();
+                    console.log('Country Id : ', selectedCountry);
+                    console.log('State Id : ', selectedState);
+                    console.log('City Id : ', selectedCity);
+                    console.log('Restaurant Name : ', restaurant);
+                    console.log('Food Name : ', foodname);
+                    console.log(rating);
     }
 
-    return (
-        <div className="container add-food-review w-full">
-            <h2 className="text-2xl text-center font-bold pb-4 text-pink-700">Add a Food review</h2>
-            <hr className="horizontal-for-profile-section" />
+                    return (
+                    <div className="container add-food-review w-full">
+                        <h2 className="text-2xl text-center font-bold pb-4 text-pink-700">Add a Food review</h2>
+                        <hr className="horizontal-for-profile-section" />
 
-            <div className="mx-auto lg:max-w-2xl p-4">
-                <form action="" onSubmit={handleReviewSubmit}>
-                    <CountrySelection onCountryChange={setSelectedCountry} />
-                    <StateSelection countryId={selectedCountry} onStateChange={setSelectedState} />
-                    <CitySelection stateId={selectedState} onCityChange={setSelectedCity} />
+                        <div className="mx-auto lg:max-w-2xl p-4">
+                            <form action="" onSubmit={handleReviewSubmit}>
+                                <CountrySelection onCountryChange={setSelectedCountry} />
+                                <StateSelection countryId={selectedCountry} onStateChange={setSelectedState} />
+                                <CitySelection stateId={selectedState} onCityChange={setSelectedCity} />
 
-                    {/* --- Select Restaurant Name --- */}
-                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                        <p className="font-semibold text-slate-800 text-center">Restaurant Name : </p>
-                        <span>
-                            <input onBlur={e => setRestaurant(e.target.value)} required type="text" placeholder="Type Restaurant Name Here " className="input input-success w-full max-w-xs  rounded-sm" />
-                        </span>
+                                {/* --- Select Restaurant Name --- */}
+                                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                                    <p className="font-semibold text-slate-800 text-center">Restaurant Name : </p>
+                                    <span>
+                                        <input onBlur={e => setRestaurant(e.target.value)} required type="text" placeholder="Type Restaurant Name Here " className="input input-success w-full max-w-xs  rounded-sm" />
+                                    </span>
+                                </div>
+
+                                {/* --- Select Food Name --- */}
+                                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                                    <p className="font-semibold text-slate-800 text-center">Food Name : </p>
+                                    <span>
+                                        <input onBlur={e => setFoodname(e.target.value)} required type="text" placeholder="Type Food Name Here  " className="input input-success w-full max-w-xs rounded-sm" />
+                                    </span>
+                                </div>
+
+                                {/* --- Select Rating --- */}
+                                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                                    <p className="font-semibold text-slate-800 text-center">Give Your Rating : </p>
+                                    <span className="flex items-center">
+                                        <div className="rating rating-lg">
+                                            <input type="radio" value="1" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                            <input type="radio" value="2" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                            <input type="radio" value="3" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                            <input type="radio" value="4" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                            <input type="radio" value="5" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                        </div>
+                                    </span>
+                                </div>
+                                <div className="lg:grid lg:grid-cols-2 justify-start items-center mt-8">
+                                    <div></div>
+                                    <div className="flex items-center">
+                                        {/* <span className="text-2xl font-bold  border border-4 border-emerald-600 w-8 h-8 flex justify-center items-center rounded-full">{rating}</span> */}
+                                        <div className="text-lg font-semibold">
+                                            {ratingImpression}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                                    <div></div>
+                                    <button className="btn btn-active btn-success w-52 hover:bg-red-800 hover:text-white border-none ">Submit Review</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-
-                    {/* --- Select Food Name --- */}
-                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                        <p className="font-semibold text-slate-800 text-center">Food Name : </p>
-                        <span>
-                            <input onBlur={e => setFoodname(e.target.value)} required type="text" placeholder="Type Food Name Here  " className="input input-success w-full max-w-xs rounded-sm" />
-                        </span>
-                    </div>
-
-                    {/* --- Select Rating --- */}
-                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                        <p className="font-semibold text-slate-800 text-center">Give Your Rating : </p>
-                        <span>
-                            <div className="rating rating-lg">
-                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                            </div>
-                        </span>
-                    </div>
-
-                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                        <div></div>
-                        <button className="btn btn-active btn-success w-52 hover:bg-red-800 hover:text-white border-none ">Submit Review</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+                    );
 };
 
-export default AddFoodReview;
+                    export default AddFoodReview;
