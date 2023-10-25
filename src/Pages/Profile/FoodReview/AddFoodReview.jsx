9e02d513db41d9c66c0d250aac7da5e71626b494
shafik720 <shafikrasel5@gmail.com>
2023-10-25 23:10:29 +0600

    successfully added food review to database with redux

--- src/Pages/Profile/FoodReview/AddFoodReview.jsx
+++ src/Pages/Profile/FoodReview/AddFoodReview.jsx
@@ -3,6 +3,7 @@ import CountrySelection from "../../../Utilites/Country&CitySelection/CountrySel
 import CitySelection from "../../../Utilites/Country&CitySelection/CitySelection";
 import StateSelection from "../../../Utilites/Country&CitySelection/StateSelection";
 import RatingImpression from "../../../Utilites/RatingImpression/RatingImpression";
+import { useAddNewFoodReviewMutation } from "../../../Redux/Features/food/foodApi";
 
 
 const AddFoodReview = () => {
@@ -23,76 +24,87 @@ const AddFoodReview = () => {
         setRating(selectedRating)
     }
 
+    // --- add a food review to database
+    const [addFoodReview, { data, isLoading, isError, error }] = useAddNewFoodReviewMutation();
+
     const handleReviewSubmit = (e) => {
-                        e.preventDefault();
-                    console.log('Country Id : ', selectedCountry);
-                    console.log('State Id : ', selectedState);
-                    console.log('City Id : ', selectedCity);
-                    console.log('Restaurant Name : ', restaurant);
-                    console.log('Food Name : ', foodname);
-                    console.log(rating);
+        e.preventDefault();
+        addFoodReview({
+            countryId : selectedCountry,
+            stateId : selectedState,
+            cityId : selectedCity,
+            restaurant,
+            foodname, 
+            rating
+        })
+        // console.log('Country Id : ', selectedCountry);
+        // console.log('State Id : ', selectedState);
+        // console.log('City Id : ', selectedCity);
+        // console.log('Restaurant Name : ', restaurant);
+        // console.log('Food Name : ', foodname);
+        // console.log(rating);
     }
 
-                    return (
-                    <div className="container add-food-review w-full">
-                        <h2 className="text-2xl text-center font-bold pb-4 text-pink-700">Add a Food review</h2>
-                        <hr className="horizontal-for-profile-section" />
+    return (
+        <div className="container add-food-review w-full">
+            <h2 className="text-2xl text-center font-bold pb-4 text-pink-700">Add a Food review</h2>
+            <hr className="horizontal-for-profile-section" />
 
-                        <div className="mx-auto lg:max-w-2xl p-4">
-                            <form action="" onSubmit={handleReviewSubmit}>
-                                <CountrySelection onCountryChange={setSelectedCountry} />
-                                <StateSelection countryId={selectedCountry} onStateChange={setSelectedState} />
-                                <CitySelection stateId={selectedState} onCityChange={setSelectedCity} />
+            <div className="mx-auto lg:max-w-2xl p-4">
+                <form action="" onSubmit={handleReviewSubmit}>
+                    <CountrySelection onCountryChange={setSelectedCountry} />
+                    <StateSelection countryId={selectedCountry} onStateChange={setSelectedState} />
+                    <CitySelection stateId={selectedState} onCityChange={setSelectedCity} />
 
-                                {/* --- Select Restaurant Name --- */}
-                                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                                    <p className="font-semibold text-slate-800 text-center">Restaurant Name : </p>
-                                    <span>
-                                        <input onBlur={e => setRestaurant(e.target.value)} required type="text" placeholder="Type Restaurant Name Here " className="input input-success w-full max-w-xs  rounded-sm" />
-                                    </span>
-                                </div>
+                    {/* --- Select Restaurant Name --- */}
+                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                        <p className="font-semibold text-slate-800 text-center">Restaurant Name : </p>
+                        <span>
+                            <input onBlur={e => setRestaurant(e.target.value)} required type="text" placeholder="Type Restaurant Name Here " className="input input-success w-full max-w-xs  rounded-sm" />
+                        </span>
+                    </div>
 
-                                {/* --- Select Food Name --- */}
-                                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                                    <p className="font-semibold text-slate-800 text-center">Food Name : </p>
-                                    <span>
-                                        <input onBlur={e => setFoodname(e.target.value)} required type="text" placeholder="Type Food Name Here  " className="input input-success w-full max-w-xs rounded-sm" />
-                                    </span>
-                                </div>
+                    {/* --- Select Food Name --- */}
+                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                        <p className="font-semibold text-slate-800 text-center">Food Name : </p>
+                        <span>
+                            <input onBlur={e => setFoodname(e.target.value)} required type="text" placeholder="Type Food Name Here  " className="input input-success w-full max-w-xs rounded-sm" />
+                        </span>
+                    </div>
 
-                                {/* --- Select Rating --- */}
-                                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                                    <p className="font-semibold text-slate-800 text-center">Give Your Rating : </p>
-                                    <span className="flex items-center">
-                                        <div className="rating rating-lg">
-                                            <input type="radio" value="1" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                            <input type="radio" value="2" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                            <input type="radio" value="3" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                            <input type="radio" value="4" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                            <input type="radio" value="5" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
-                                        </div>
-                                    </span>
-                                </div>
+                    {/* --- Select Rating --- */}
+                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                        <p className="font-semibold text-slate-800 text-center">Give Your Rating : </p>
+                        <span className="flex items-center">
+                            <div className="rating rating-lg">
+                                <input type="radio" value="1" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" value="2" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" value="3" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" value="4" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" value="5" onChange={handleRating} name="rating-4" className="mask mask-star-2 bg-orange-400" />
+                            </div>
+                        </span>
+                    </div>
 
-                                {/* --- Rating impression --- */}
-                                <div className="lg:grid lg:grid-cols-2 justify-start items-center mt-8">
-                                    <div></div>
-                                    <div className="flex items-center">
-                                    {/*  this component will generate animated emoticon according to the user's rating selection to give the user a better ui */}
-                                        <div className="text-lg font-semibold">
-                                            <RatingImpression selectedRating={rating} />
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
-                                    <div></div>
-                                    <button className="btn btn-active btn-success w-52 hover:bg-red-800 hover:text-white border-none ">Submit Review</button>
-                                </div>
-                            </form>
+                    {/* --- Rating impression --- */}
+                    <div className="lg:grid lg:grid-cols-2 justify-start items-center mt-8">
+                        <div></div>
+                        <div className="flex items-center">
+                            {/*  this component will generate animated emoticon according to the user's rating selection to give the user a better ui */}
+                            <div className="text-lg font-semibold">
+                                <RatingImpression selectedRating={rating} />
+                            </div>
                         </div>
                     </div>
-                    );
+
+                    <div className="lg:grid lg:grid-cols-2 justify-center items-center mt-8">
+                        <div></div>
+                        <button className="btn btn-active btn-success w-52 hover:bg-red-800 hover:text-white border-none ">Submit Review</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
-                    export default AddFoodReview;
+export default AddFoodReview;
--- src/Redux/Features/Api/apiSlice.js
+++ src/Redux/Features/Api/apiSlice.js
@@ -5,7 +5,8 @@ export const apiSlice = createApi({
     reducerPath : 'api' , 
     baseQuery : fetchBaseQuery({
         // baseUrl : import.meta.env.VITE_APP_REDUX_BASE_QUERY
-        baseUrl : 'https://server-for-foodbank.vercel.app'
+        // baseUrl : 'https://server-for-foodbank.vercel.app',
+        baseUrl : 'http://localhost:4000',
     }),
     tagTypes : [] , 
     endpoints : (builder) => ({}) 
