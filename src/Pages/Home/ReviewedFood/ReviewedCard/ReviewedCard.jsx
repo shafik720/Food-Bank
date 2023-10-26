4d0ea085599d4b3002bd47a6d8e35f812f4231c8
shafik720 <shafikrasel5@gmail.com>
2023-10-26 23:41:36 +0600

    get single city's data from server with redux

--- src/Pages/Home/ReviewedFood/ReviewedCard/ReviewedCard.jsx
+++ src/Pages/Home/ReviewedFood/ReviewedCard/ReviewedCard.jsx
@@ -1,19 +1,29 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import RatingImpression from "../../../../Utilites/RatingImpression/RatingImpression";
+import { useEffect } from "react";
+import { useGetSingleCityQuery } from "../../../../Redux/Features/city/cityApi";
 
 
 const ReviewedCard = ({ data }) => {
-    const { foodname, imgUrl, rating, restaurant } = data.data;
-    console.log(data);
+    const { foodname, imgUrl, rating, restaurant, countryId, stateId, cityId } = data.data;
+
+
+    // --- getting single city details with redux
+    const {data : singleCity} = useGetSingleCityQuery(cityId);
+
+    
     return (
         <div className="vegan-card bg-white w-52 pb-9 pt-4 px-4">
 
+            {/* === Food Name === */}
             <div className="w-40 h-40 overflow-hidden flex justify-center items-start mx-auto">
                 <img className=' w-full ' src={imgUrl || 'https://i.ibb.co/k1ydWxp/recipes-21-700x600.jpg'} alt="" />
             </div>
 
             <h2 className='text-slate-900 font-semibold text-lg '>{foodname}</h2>
+
+            {/* === rating section starts === */}
             <div className="rating-div flex justify-center items-center mt-4 ">
                 <p className='text-black'>Rating : </p>
                 <div className="bg-orange-400 flex justify-center items-center gap-2 px-2 ms-2">
@@ -24,6 +34,26 @@ const ReviewedCard = ({ data }) => {
             <div className="text-white border-white mt-3 border-4 flex justify-center items-center rounded-full bg-emerald-500">
                 <RatingImpression selectedRating={rating} />
             </div>
+            {/* === Rating Section Starts === */}
+
+            {/* === Restaurant Details Starts === */}
+            <div className="restaurant-details-div text-black mt-5 text-start">
+                <div className="">
+                    <p>Restaurant :</p>
+                    <p className="font-bold text-xl capitalize"> {restaurant} </p>
+                </div>
+                <div>
+                    Country : {}
+                </div>
+                <div>
+                    State : {}
+                </div>
+                <div>
+                    <p>City :</p>
+                    <p className="font-bold "> {singleCity?.city_name} </p>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/Redux/Features/city/cityApi.js
+++ src/Redux/Features/city/cityApi.js
@@ -3,11 +3,18 @@ import { apiSlice } from "../Api/apiSlice";
 
 const cityApi = apiSlice.injectEndpoints({
     endpoints : (builder) => ({
+
+        // --- get all country details
         getCityList : builder.query({
             query : (stateId) => `/getCityList/${stateId}` 
+        }),
+
+        // --- get single city details
+        getSingleCity : builder.query({
+            query : (cityId)=> `/getSingleCity/${cityId}`
         })
     })
 })
 
 
-export const {useGetCityListQuery} = cityApi
+export const {useGetCityListQuery, useGetSingleCityQuery} = cityApi
