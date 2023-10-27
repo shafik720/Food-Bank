5731b2c7f888383fd7595f4a79a3c7fdf922a392
shafik720 <shafikrasel5@gmail.com>
2023-10-28 02:06:54 +0600

    creating scroll up button

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,9 +4,30 @@ import CategoryDiv from "../CategoryDiv/CategoryDiv";
 import VeganDiv from "../VeganDiv/VeganDiv";
 import WeeklyReviewer from "../WeeklyReviewer/WeeklyReviewer";
 import ReviewedFood from "../ReviewedFood/ReviewedFood";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUp } from "@fortawesome/free-solid-svg-icons";
 
 
 const Home = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top : 0,
+            behavior : 'smooth'
+        })
+    }
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true)
+        }
+        else if (scrolled <= 300) {
+            setVisible(false)
+        }
+    };
+    window.addEventListener('scroll', toggleVisible);
     return (
         <div>
             <Helmet>
@@ -18,6 +39,8 @@ const Home = () => {
             {/* <VeganDiv></VeganDiv> */}
             <ReviewedFood></ReviewedFood>
             <WeeklyReviewer></WeeklyReviewer>
+
+            <button className={` fixed  bottom-7 z-50 btn  transition-all ease-in-out delay-100 text-2xl ${visible ? 'right-7' : '-right-32'}`} onClick={scrollToTop}><FontAwesomeIcon icon={faCircleUp} /></button>
 
             <p className="my-60">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, ipsam. Veritatis itaque doloribus obcaecati non fugiat, earum corporis eos! Distinctio atque illum nostrum facere, odio dicta quas. Sed, incidunt officia? Omnis cum necessitatibus possimus voluptate, magnam unde vel eligendi nihil vero labore? Obcaecati porro repellendus deserunt non quibusdam, deleniti cupiditate.</p>
         </div>
---
+++ src/Utilites/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const ScrollToTop = () => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true)
+        }
+        else if (scrolled <= 300) {
+            setVisible(false)
+        }
+    };
+    window.addEventListener('scroll', toggleVisible);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+            /* you can also use 'auto' behaviour 
+               in place of 'smooth' */
+        });
+    };
+    return (
+        <Button type="button">Button</Button>
+    );
+};
+
+export default ScrollToTop;
