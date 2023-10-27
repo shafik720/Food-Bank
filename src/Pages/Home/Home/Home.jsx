bd24f5dd5e173427eeea02709526f70e2c036237
shafik720 <shafikrasel5@gmail.com>
2023-10-28 02:28:56 +0600

    created scroll up button

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -7,40 +7,24 @@ import ReviewedFood from "../ReviewedFood/ReviewedFood";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUp } from "@fortawesome/free-solid-svg-icons";
+import ScrollToTop from "../../../Utilites/ScrollToTop/ScrollToTop";
 
 
 const Home = () => {
-    const scrollToTop = () => {
-        window.scrollTo({
-            top : 0,
-            behavior : 'smooth'
-        })
-    }
-    const [visible, setVisible] = useState(false);
-
-    const toggleVisible = () => {
-        const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300) {
-            setVisible(true)
-        }
-        else if (scrolled <= 300) {
-            setVisible(false)
-        }
-    };
-    window.addEventListener('scroll', toggleVisible);
+    
     return (
         <div>
             <Helmet>
                 <title>Food Bank</title>
                 <link rel="icon" type="image/svg+xml" href="https://i.ibb.co/NtS7rNm/cutlery.png" />
             </Helmet>
+            <ScrollToTop />
             <Banner></Banner>
             <CategoryDiv></CategoryDiv>
             {/* <VeganDiv></VeganDiv> */}
             <ReviewedFood></ReviewedFood>
             <WeeklyReviewer></WeeklyReviewer>
 
-            <button className={` fixed  bottom-7 z-50 btn  transition-all ease-in-out delay-100 text-2xl ${visible ? 'right-7' : '-right-32'}`} onClick={scrollToTop}><FontAwesomeIcon icon={faCircleUp} /></button>
 
             <p className="my-60">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, ipsam. Veritatis itaque doloribus obcaecati non fugiat, earum corporis eos! Distinctio atque illum nostrum facere, odio dicta quas. Sed, incidunt officia? Omnis cum necessitatibus possimus voluptate, magnam unde vel eligendi nihil vero labore? Obcaecati porro repellendus deserunt non quibusdam, deleniti cupiditate.</p>
         </div>
--- src/Utilites/ScrollToTop/ScrollToTop.jsx
+++ src/Utilites/ScrollToTop/ScrollToTop.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import { faCircleUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 
 const ScrollToTop = () => {
+
     const [visible, setVisible] = useState(false);
 
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
     const toggleVisible = () => {
         const scrolled = document.documentElement.scrollTop;
         if (scrolled > 300) {
@@ -13,17 +22,10 @@ const ScrollToTop = () => {
         }
     };
     window.addEventListener('scroll', toggleVisible);
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-            /* you can also use 'auto' behaviour 
-               in place of 'smooth' */
-        });
-    };
     return (
-        <Button type="button">Button</Button>
+        <div>
+            <button className={` fixed  bottom-7 z-50 btn  transition-all ease-in-out delay-100 text-lg md:text-2xl ${visible ? 'lg:right-7 right-3' : '-right-32'}`} onClick={scrollToTop}><FontAwesomeIcon icon={faCircleUp} /></button>
+        </div>
     );
 };
 
