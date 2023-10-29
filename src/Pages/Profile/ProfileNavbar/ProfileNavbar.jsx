2e67a61fa399a3ee7936fbbc2d3d0159ac2e2ae5
shafik720 <shafikrasel5@gmail.com>
2023-10-29 22:39:14 +0600

    fixing profile menu navbar for mobile view

--- src/Pages/Profile/ProfileNavbar/ProfileNavbar.jsx
+++ src/Pages/Profile/ProfileNavbar/ProfileNavbar.jsx
@@ -1,31 +1,46 @@
 import { NavLink } from 'react-router-dom';
 // import './ProfileNavbar.css';
-import './ProfileNavbar2.css';
+import './ProfileNavbar.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../../Utilites/Firebase Auth/firebase.inti';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const ProfileNavbar = () => {
 
     // --- getting user information
-    const [user,loading, error] = useAuthState(auth) ; 
-    
+    const [user, loading, error] = useAuthState(auth);
+
+    const [activeMobile, setActiveMobile] = useState(false);
+
+    // const handleMobileMenu = () => {
+    //     const menu = document.querySelector('.profile-navbar-div');
+    //     menu.classList.add('activeMobile');
+    // }
+
     return (
-        <div className="profile-navbar-div">
-            <ul>
-                {/* <NavLink to='/profile/profileInfo' activeClassName="active">about</NavLink> */}
-                <li>
-                    <NavLink to='/profile/profileInfo' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span special-radius1"><img src={user?.photoURL || 'https://i.ibb.co/g99nZMz/user.png'} alt="" /></span></span> <span className='menu-text'>Profile</span></NavLink>
-                </li>
-                <li>
-                    <NavLink to='/profile/editProfile' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span special-radius2"><img src="https://i.ibb.co/5WDXr6R/user-avatar-1.png" alt="" /></span></span> <span className='menu-text'>Edit Profile</span></NavLink>
-                </li>
-                <li>
-                    <NavLink to='/profile/addReview' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span special-radius"><img src="https://i.ibb.co/pXgKmgC/restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle.png" alt="" /></span></span> <span className='menu-text'>Add a Food Review</span></NavLink>
-                </li>
-                {/* <li>
-                    <NavLink to='/profile/editProfile' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span"><img src="https://i.ibb.co/wyyc0S0/user-avatar.png" alt="" /></span></span> <span className='menu-text'>Edit Profile</span></NavLink>
-                </li> */}
-            </ul>
+        <div className="">
+            <div className={`profile-navbar-div ${activeMobile ? 'activeMobile' : ''}`}>
+                <ul>
+                    {/* <NavLink to='/profile/profileInfo' activeClassName="active">about</NavLink> */}
+                    <li>
+                        <NavLink to='/profile/profileInfo' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span special-radius1"><img src={user?.photoURL || 'https://i.ibb.co/g99nZMz/user.png'} alt="" /></span></span> <span className='menu-text'>Profile</span></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/profile/editProfile' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span special-radius2"><img src="https://i.ibb.co/5WDXr6R/user-avatar-1.png" alt="" /></span></span> <span className='menu-text'>Edit Profile</span></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/profile/addReview' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span special-radius"><img src="https://i.ibb.co/pXgKmgC/restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle.png" alt="" /></span></span> <span className='menu-text'>Add a Food Review</span></NavLink>
+                    </li>
+                    {/* <li>
+                <NavLink to='/profile/editProfile' activeClassName="menu-active" className='flex flex-col justify-center items-center gap-2  relative'> <span className='menu-outer-span'> <span className="menu-inner-span"><img src="https://i.ibb.co/wyyc0S0/user-avatar.png" alt="" /></span></span> <span className='menu-text'>Edit Profile</span></NavLink>
+            </li> */}
+                </ul>
+            </div>
+            <div className={`mobile-menu-icon ${activeMobile ? 'activeMobile' : ''}`} onClick={()=>setActiveMobile(!activeMobile)}>
+                <p><FontAwesomeIcon icon={faBars} /></p>
+            </div>
         </div>
     );
 };
